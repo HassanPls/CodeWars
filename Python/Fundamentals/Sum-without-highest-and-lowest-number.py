988dc5e628046caae444618ae05c8b5bb6cc548e
HassanPls <plmvfilho@gmail.com>
2026-05-27 14:08:21 -0300

    Fundamentals Python (003)

--- Python/Fundamentals/Sum-without-highest-and-lowest-number.py
+++ Python/Fundamentals/Sum-without-highest-and-lowest-number.py
@@ -4,24 +4,33 @@ The highest or lowest element respectively is a single element at each edge, eve
 
 Mind the input validation. """
 
-def sum_array(arr):
+
+# First try
+""" def sum_array(arr):
     if not arr:
         return 0
     
     sum = 0
-    highest = 0
-    lowest = 0
-    for x in arr:
-        if (x < lowest):
-            sum = sum + lowest
-            lowest = x
-            break
+    highest = lowest = arr[0]
 
-        if (x > highest):
-            sum = sum + highest
+    for i, x in enumerate(arr[1:]):
+        if (x < lowest):
+            if(i > 0): sum = sum + lowest
+            lowest = x
+            continue
+
+        if (x >= highest):
+            if(i > 0): sum = sum + highest
             highest = x
-            break
+            continue
 
         sum = sum + x
 
-    return sum
+    return sum """
+
+#Second try
+def sum_array(arr):
+    if not arr or len(arr) <= 2:
+        return 0
+    
+    return sum(arr) - max(arr) - min(arr)
